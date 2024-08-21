6855577b3a020d06d45203860af24f461f2cf7de
jeongbaebang <jeongbaebang_dev@naver.com>
2024-08-21 18:06:10 +0900

    Add files via upload

---
+++ 0821/typescript/dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+{
+    let name = 'kim';
+}
+function add(a, b) {
+    return a + b;
+}
+function isEven(num) {
+    return num % 2 === 0;
+}
---
+++ 0821/typescript/src/main.ts
@@ -0,0 +1,11 @@
+{
+  let name: string = 'kim';
+}
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function isEven(num: number): boolean {
+  return num % 2 === 0;
+}
