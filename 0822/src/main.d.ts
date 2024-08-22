a93a85edeb3c3aee75570fe632531819df2b9870
jeongbaebang <jeongbaebang_dev@naver.com>
2024-08-22 22:47:11 +0900

    Add files via upload

---
+++ 0822/src/main.d.ts
@@ -0,0 +1,8 @@
+declare const user: {
+    name: string;
+    age: number;
+    ping: undefined;
+    arr: number[];
+};
+declare const arr: [number, number];
+declare const add: (a: number, b: number) => number;
